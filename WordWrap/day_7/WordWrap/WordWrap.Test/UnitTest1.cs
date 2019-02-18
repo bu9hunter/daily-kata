d71fbbb32776119ddbb412a6c78e74d109a19ef3
bu9hunter <ggobok@naver.com>
2019-02-18 09:50:17 +0900

    Day7 commit

---
+++ WordWrap/day_7/WordWrap/WordWrap.Test/UnitTest1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WordWrapper;
+
+namespace WordWrap.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void WordWrapper_인스턴스_만들기()
+        {
+            WordMachine wordMachine = new WordMachine();
+        }
+
+        [TestMethod]
+        public void 한단어_줄바꿈하기()
+        {
+            string inputString = "test";
+            string expectedString = "test";
+            string actualString = "";
+
+            WordMachine wordMachine = new WordMachine();
+
+            actualString = wordMachine.wrap(inputString, 7);
+
+            Assert.AreEqual(expectedString, actualString);
+        }
+
+        [TestMethod]
+        public void 두단어_줄바꿈하기()
+        {
+            string inputString = "hello world";
+            string expectedString = "hello--world";
+            string actualString = "";
+
+            WordMachine wordMachine = new WordMachine();
+
+            actualString = wordMachine.wrap(inputString, 7);
+
+            Assert.AreEqual(expectedString, actualString);
+        }
+    }
+}
---
+++ WordWrap/day_7/WordWrap/WordWrapper/WordMachine.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace WordWrapper
+{
+    public class WordMachine
+    {
+        public string wrap(string text, int length)
+        {
+            string tempString = "";
+            int spacePosition;
+            int totalLength = text.Length;
+
+            tempString = text.Substring(0, length);
+            spacePosition = tempString.LastIndexOf(" ");
+
+            if (spacePosition == -1)
+            {
+                return text;
+            }
+            else
+            {
+                if (spacePosition < length)
+                {
+                }
+            }
+
+            return text;
+        }
+    }
+}
