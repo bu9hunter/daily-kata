8c702eef52712367fbefe0d6855b295c12e659ee
bu9hunter <ggobok@naver.com>
2019-03-07 02:40:44 +0900

    demo

---
+++ WordWrap/demo/WordWrap/WordWrap.Test/UnitTest1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WordWrap.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void 인스턴스_만들기()
+        {
+            WordWrapper wordWrapper = new WordWrapper();
+        }
+
+        [TestMethod]
+        public void 한단어_줄바꿈하기()
+        {
+            string inputText = "test";
+            string expectedText = "test";
+            string actualText = "";
+            int limit = 7;
+
+            WordWrapper wordWrapper = new WordWrapper();
+
+            actualText = wordWrapper.wrap(inputText, limit);
+
+            Assert.AreEqual(expectedText, actualText);
+        }
+
+        [TestMethod]
+        public void 두단어_줄바꿈하기()
+        {
+            string inputText = "hello world";
+            string expectedText = "hello--world";
+            string actualText = "";
+            int limit = 7;
+
+            WordWrapper wordWrapper = new WordWrapper();
+
+            actualText = wordWrapper.wrap(inputText, limit);
+
+            Assert.AreEqual(expectedText, actualText);
+        }
+
+        [TestMethod]
+        public void 한줄에_많은단어_줄바꿈하기()
+        {
+            string inputText = "a lot of words for a single line";
+            string expectedText = "a lot of--words for--a single--line";
+            string actualText = "";
+            int limit = 10;
+
+            WordWrapper wordWrapper = new WordWrapper();
+
+            actualText = wordWrapper.wrap(inputText, limit);
+
+            Assert.AreEqual(expectedText, actualText);
+        }
+
+        [TestMethod]
+        public void 테스트문장_줄바꿈하기()
+        {
+            string inputText = "this is a test";
+            string expectedText = "this--is a--test";
+            string actualText = "";
+            int limit = 4;
+
+            WordWrapper wordWrapper = new WordWrapper();
+
+            actualText = wordWrapper.wrap(inputText, limit);
+
+            Assert.AreEqual(expectedText, actualText);
+        }
+
+        [TestMethod]
+        public void 긴단어_줄바꿈하기()
+        {
+            string inputText = "a longword";
+            string expectedText = "a long--word";
+            string actualText = "";
+            int limit = 6;
+
+            WordWrapper wordWrapper = new WordWrapper();
+
+            actualText = wordWrapper.wrap(inputText, limit);
+
+            Assert.AreEqual(expectedText, actualText);
+        }
+
+        [TestMethod]
+        public void 정말긴단어_줄바꿈하기()
+        {
+            string inputText = "areallylongword";
+            string expectedText = "areall--ylongw--ord";
+            string actualText = "";
+            int limit = 6;
+
+            WordWrapper wordWrapper = new WordWrapper();
+
+            actualText = wordWrapper.wrap(inputText, limit);
+
+            Assert.AreEqual(expectedText, actualText);
+        }
+
+        [DataRow("word", "word", 6)]
+        [DataRow("word", "word", 5)]
+        [DataRow("word", "word", 4)]
+        [DataRow("wor--d", "word", 3)]
+        //[DataRow("word--word", "word word", 4)]
+        [DataRow("word--word", "word word", 5)]
+        [DataRow("word--word", "word word", 7)]
+        [DataRow("word word--word", "word word word", 10)]
+        [DataRow("word word--word", "word word word", 12)]
+        [DataRow("word--word--word", "word word word", 5)]
+        [DataRow("a lot of--words", "a lot of words", 10)]
+        [DataRow("a lot of--words for--a single", "a lot of words for a single", 10)]
+        [DataTestMethod]
+        public void 더많은단어(string Expected, string Given, int Length)
+        {
+            WordWrapper wordWrapper = new WordWrapper();
+
+            Assert.AreEqual(Expected, wordWrapper.wrap(Given, Length));
+        }
+    }
+}
---
+++ WordWrap/demo/WordWrap/WordWrap.Test/WordWrapper.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace WordWrap.Test
+{
+    public class WordWrapper
+    {
+        public WordWrapper()
+        {
+        }
+
+        internal string wrap(string inputText, int limit)
+        {
+            int lastSpacePosition;
+            int firstSpacePosition;
+            int wordSize;
+            string wrappedText = "";
+            string remainText = "";
+
+            if (inputText.Length <= limit)
+            {
+                return inputText;
+            }
+
+            remainText = inputText;
+
+            while (remainText.Length > limit)
+            {
+                lastSpacePosition = remainText.Substring(0, limit).LastIndexOf(' ');
+                firstSpacePosition = remainText.Substring(limit - 1).IndexOf(' ');
+
+                if (lastSpacePosition == -1)
+                {
+                    lastSpacePosition = 0;
+                }
+
+                if (firstSpacePosition == -1)
+                {
+                    firstSpacePosition = remainText.Length;
+                } else
+                {
+                    firstSpacePosition = limit - 1 + firstSpacePosition;
+                }
+
+                wordSize = firstSpacePosition - lastSpacePosition;
+
+                if (wordSize > limit || lastSpacePosition == 0)
+                {
+                    wrappedText += remainText.Substring(0, limit);
+                    wrappedText += "--";
+                    remainText = remainText.Substring(limit).TrimStart();
+                    continue;
+                }
+
+                if (lastSpacePosition < limit)
+                {
+                    wrappedText += remainText.Substring(0, lastSpacePosition);
+                    wrappedText += "--";
+                    remainText = remainText.Substring(lastSpacePosition + 1);
+                }
+            }            
+
+            return wrappedText + remainText;
+        }
+    }
+}
