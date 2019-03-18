33e14b2ad48a7b154509e354655a5932126eb5a0
bu9hunter <ggobok@naver.com>
2019-03-18 09:56:35 +0900

    bowling kata day_1

---
+++ BowlingGame/day_1/BowlingGame/BowlingGame.Test/UnitTest1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BowlingGame.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Game인스턴스만들기()
+        {
+            Game game = new Game();
+        }
+
+        [TestMethod]
+        public void 점수_확인하기()
+        {
+            int score;
+
+            Game game = new Game();
+
+            score = game.score();
+
+            Assert.AreEqual(0, score);
+        }
+
+        [TestMethod]
+        public void 첫번째시도_핀1개_치고_점수_확인하기()
+        {
+            int expectedScore = 1;
+            int actualScore;
+            
+            Game game = new Game();
+
+            game.roll(1);
+            actualScore = game.score();
+
+            Assert.AreEqual(expectedScore, actualScore);
+        }
+
+        [TestMethod]
+        public void 첫번째시도_핀1개_두번째시도_핀3개_치고_점수_확인하기()
+        {
+            int expectedScore = 4;
+            int actualScore;
+
+            Game game = new Game();
+
+            // frame #1
+            game.roll(1);
+            game.roll(3);
+            actualScore = game.score();
+
+            Assert.AreEqual(expectedScore, actualScore);
+        }
+
+        [TestMethod]
+        public void 두번째_프레임까지_진행한_점수_확인하기()
+        {
+            int expectedScore = 10;
+            int actualScore;
+
+            Game game = new Game();
+
+            // frame #1
+            game.roll(1);
+            game.roll(3);
+            // frame #2
+            game.roll(5);
+            game.roll(1);
+            actualScore = game.score();
+
+            Assert.AreEqual(expectedScore, actualScore);
+        }
+    }
+}
---
+++ BowlingGame/day_1/BowlingGame/BowlingGame/Game.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BowlingGame
+{
+    public class Game
+    {
+        int m_pins;
+
+        public int score()
+        {
+            return m_pins;
+        }
+
+        public void roll(int pins)
+        {
+            m_pins += pins;
+        }
+    }
+}
