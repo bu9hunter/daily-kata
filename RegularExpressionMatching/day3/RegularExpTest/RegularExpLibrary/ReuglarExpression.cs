87fdec419602de610f0fe438ea0d9593a3952a90
bu9hunter <ggobok@naver.com>
2019-06-12 09:31:31 +0900

    day3 - Regular exp

---
+++ RegularExpressionMatching/day3/RegularExpTest/RegularExpLibrary/ReuglarExpression.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace RegularExpLibrary
+{
+    public class RegularExpression
+    {
+        public RegularExpression()
+        {
+        }
+
+        public bool isMatch(string text, string pattern)
+        {
+            int idx = 0;
+            string temp;
+
+            if (hasWildCardChar(pattern))
+            {
+                foreach (char element in pattern)
+                {
+                    
+                    if (element.Equals("."))
+                    {
+                        
+                    }
+                    else if (element.Equals("*"))
+                    {
+
+                    }
+                    else
+                    {
+
+                    }
+
+                    idx++;
+                }
+                return false;
+            }
+            else
+            {
+                return text.Equals(pattern);
+            }
+        }
+
+        private static bool hasWildCardChar(string pattern)
+        {
+            return pattern.Contains(".") || pattern.Contains("*");
+        }
+    }
+}
---
+++ RegularExpressionMatching/day3/RegularExpTest/RegularExpTest/RegularExpTestSuite.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RegularExpLibrary;
+
+namespace RegularExpTest
+{
+    [TestClass]
+    public class RegularExpTestSuite
+    {
+        RegularExpression regExp;
+
+        [TestInitialize]
+        public void testInit()
+        {
+            regExp = new RegularExpression();
+        }
+
+        [TestMethod]
+        public void _0_MakeInstance()
+        {
+            RegularExpression regExp = new RegularExpression();
+        }
+
+        [TestMethod]
+        public void _1_글자패턴1개_매칭확인하기()
+        {
+            //given
+            string text = "a";
+            string pattern = "a";
+
+            //when
+            bool result = regExp.isMatch(text, pattern);
+
+            //then
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void _2_글자패턴2개_매칭확인하기()
+        {
+            //given
+            string text = "aa";
+            string pattern = "aa";
+
+            //when
+            bool result = regExp.isMatch(text, pattern);
+
+            //then
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void _3_글자패턴3개_매칭확인하기()
+        {
+            //given
+            string text = "abc";
+            string pattern = "abc";
+
+            //when
+            bool result = regExp.isMatch(text, pattern);
+
+            //then
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void _4_Example1()
+        {
+            //given
+            string text = "aa";
+            string pattern = "a";
+
+            //when
+            bool result = regExp.isMatch(text, pattern);
+
+            //then
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void _5_와일드카드패턴1개_매칭확인하기()
+        {
+            //given
+            string text = "a";
+            string pattern = ".";
+
+            //when
+            bool result = regExp.isMatch(text, pattern);
+
+            //then
+            Assert.IsTrue(result);
+        }
+    }
+}
