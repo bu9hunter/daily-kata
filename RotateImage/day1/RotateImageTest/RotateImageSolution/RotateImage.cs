accf39e635775265799f6ac849b4dcf01dffa432
bu9hunter <ggobok@naver.com>
2019-07-11 09:59:07 +0900

    RoateImage - day3

--- RotateImage/day1/RotateImageTest/RotateImageSolution/RotateImage.cs
+++ RotateImage/day1/RotateImageTest/RotateImageSolution/RotateImage.cs
@@ -10,15 +10,38 @@ namespace RotateImageSolution
 
         public void Rotate(ref int[,] inputMatrix)
         {
-            // Set Contents
-            inputMatrix[2, 2] = inputMatrix[0, 2];
+            int[] temp = new int[3];
+            bool clearOne = false;
+            bool clearTwo = false;
+
+            if (inputMatrix[1, 0] == 0) clearOne = true;
+            if (inputMatrix[2, 0] == 0) clearTwo = true;
+
+            // Backup
+            temp[2] = inputMatrix[2, 2];
+            temp[1] = inputMatrix[1, 2];
+            temp[0] = inputMatrix[0, 2];
+
+            // Set Column #3
+            inputMatrix[2, 2] = temp[0];
             inputMatrix[1, 2] = inputMatrix[0, 1];
             inputMatrix[0, 2] = inputMatrix[0, 0];
 
+            // Set Column #2
+            inputMatrix[2, 1] = temp[1];
+            inputMatrix[1, 1] = inputMatrix[1, 1];
+            inputMatrix[0, 1] = inputMatrix[1, 0];
+
+            // Set Column #1
+            inputMatrix[2, 0] = temp[2];
+            inputMatrix[1, 0] = inputMatrix[2, 1];
+            inputMatrix[0, 0] = inputMatrix[2, 0];
+
             // Clear Contents
-            inputMatrix[0, 0] = 0;
-            inputMatrix[0, 1] = 0;
-            inputMatrix[0, 2] = 0;
+            if (clearOne) inputMatrix[0, 0] = 0;
+            if (clearTwo) inputMatrix[1, 0] = 0;
+
+            //inputMatrix[0, 2] = 0;
         }
     }
 }
--- RotateImage/day1/RotateImageTest/RotateImageTest/UnitTest1.cs
+++ RotateImage/day1/RotateImageTest/RotateImageTest/UnitTest1.cs
@@ -36,5 +36,53 @@ namespace RotateImageTest
 
             CollectionAssert.AreEqual(rotateMatrix, inputMatrix);
         }
+
+        [TestMethod]
+        public void Rotate2RowBy90degrees()
+        {
+            int[,] inputMatrix = new int[,]
+            {
+                { 1, 2, 3 },
+                { 4, 5, 6 },
+                { 0, 0, 0 }
+            };
+
+            int[,] rotateMatrix = new int[,]
+            {
+                { 0, 4, 1 },
+                { 0, 5, 2 },
+                { 0, 6, 3 }
+            };
+
+            RotateImage rotateImage = new RotateImage();
+
+            rotateImage.Rotate(ref inputMatrix);
+
+            CollectionAssert.AreEqual(rotateMatrix, inputMatrix);
+        }
+
+        [TestMethod]
+        public void Rotate3RowBy90degrees()
+        {
+            int[,] inputMatrix = new int[,]
+            {
+                { 1, 2, 3 },
+                { 4, 5, 6 },
+                { 7, 8, 9 }
+            };
+
+            int[,] rotateMatrix = new int[,]
+            {
+                { 7, 4, 1 },
+                { 8, 5, 2 },
+                { 9, 6, 3 }
+            };
+
+            RotateImage rotateImage = new RotateImage();
+
+            rotateImage.Rotate(ref inputMatrix);
+
+            CollectionAssert.AreEqual(rotateMatrix, inputMatrix);
+        }
     }
 }
