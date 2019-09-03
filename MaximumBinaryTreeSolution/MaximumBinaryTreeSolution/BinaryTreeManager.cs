0ae34943cce53dd7601f1cab29226aaa6fcd678c
bu9hunter <ggobok@naver.com>
2019-09-03 10:12:16 +0900

    Maximum Binary Tree - 2Day

--- MaximumBinaryTreeSolution/MaximumBinaryTreeSolution/BinaryTreeManager.cs
+++ MaximumBinaryTreeSolution/MaximumBinaryTreeSolution/BinaryTreeManager.cs
@@ -1,9 +1,22 @@
-﻿namespace MaximumBinaryTreeSolution
+﻿using System;
+using System.Linq;
+
+namespace MaximumBinaryTreeSolution
 {
     public class BinaryTreeManager
     {
         public BinaryTreeManager()
         {
+        }
+
+        public int getIndexOfMaxValue(int[] tree)
+        {
+            return tree.ToList().IndexOf(tree.Max());
+        }
+
+        public void splitTreeByMaxValue(int[] mainTree, int maxIndex, int[] leftTree, int[] rightTree)
+        {
+            throw new NotImplementedException();
         }
     }
 }
--- MaximumBinaryTreeSolution/MaximumBinaryTreeTest/UnitTest1.cs
+++ MaximumBinaryTreeSolution/MaximumBinaryTreeTest/UnitTest1.cs
@@ -12,5 +12,36 @@ namespace MaximumBinaryTreeTest
         {
             BinaryTreeManager btm = new BinaryTreeManager();
         }
+
+        [TestMethod]
+        public void findMaxIntegerIndex()
+        {
+            int[] mainTree = new int[] { 3, 2, 1, 6, 0, 5 };
+
+            BinaryTreeManager btm = new BinaryTreeManager();
+
+            int maxIndex = btm.getIndexOfMaxValue(mainTree);
+
+            Assert.AreEqual(3, maxIndex);
+        }
+
+        [TestMethod]
+        public void splitArrayByMaxInteger()
+        {
+            int[] mainTree = new int[] { 3, 2, 1, 6, 0, 5 };
+            int[] expectedLeftTree = new int[] { 3, 2, 1 };
+            int[] expectedRightTree = new int[] { 0, 5 };
+            int[] leftTree = new int[100];
+            int[] rightTree = new int[100];
+
+            BinaryTreeManager btm = new BinaryTreeManager();
+
+            int maxIndex = btm.getIndexOfMaxValue(mainTree);
+
+            btm.splitTreeByMaxValue(mainTree, maxIndex, leftTree, rightTree);
+
+            CollectionAssert.AreEqual(expectedLeftTree, leftTree);
+            CollectionAssert.AreEqual(expectedRightTree, rightTree);
+        }
     }
 }
